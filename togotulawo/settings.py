371072c7c597067fadbaf8024ff53f6f3cb1ea4e
jojo228 <49480316+jojo228@users.noreply.github.com>
2023-01-16 19:28:28 +0000

    Email Account verification

--- togotulawo/settings.py
+++ togotulawo/settings.py
@@ -295,9 +295,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'togotulawo@gmail.com' #Enter your email
-EMAIL_HOST_PASSWORD = 'L1kABo55' #Enter the password to the email
+AUTHENTICATION_BACKENDS = [
+    'account.backends.EmailBackend'
+]
+
+# SMTP Configuration (Its working fine, just add the mail and its password)
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv("EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL")
+
+PASSWORD_RESET_TIMEOUT = 14400
