0cdfb0cd91857a4737681d0ce92f61963208bf77
jojo228 <49480316+jojo228@users.noreply.github.com>
2023-03-29 18:43:53 +0000

    updates agin

--- togotulawo/settings.py
+++ togotulawo/settings.py
@@ -213,7 +213,7 @@ ROOT_URLCONF = 'togotulawo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
