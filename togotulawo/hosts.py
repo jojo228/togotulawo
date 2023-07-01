abc0836af55efed8a8b83b5a419d6bdef081e184
jojo228 <49480316+jojo228@users.noreply.github.com>
2023-07-01 16:40:06 +0000

    test 3

--- togotulawo/hosts.py
+++ togotulawo/hosts.py
@@ -5,4 +5,7 @@ from django.conf import settings
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
     host(r'gedus', 'main.urls', name='main'),
+    host(r'account', 'account.urls', name='account'),
+    host(r'dashboard', 'dashboard.urls', name='dashboard'),
+    host(r'entreprise', 'entreprise.urls', name='entreprise'),
 )
