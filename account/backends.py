77e43f0f788e76eac183fef8930b2279d0170176
jojo228 <49480316+jojo228@users.noreply.github.com>
2023-07-05 21:22:56 +0000

    tests

--- account/backends.py
+++ account/backends.py
@@ -1,18 +1,28 @@
-from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.backends import ModelBackend, BaseBackend
 from django.contrib.auth import get_user_model
 from django.db.models import Q
+from account.models import User
+
 
 UserModel = get_user_model()
 
-class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
+class EmailBackend(BaseBackend):
+    def authenticate(self, request, **kwargs):
         try:
-            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
-        except UserModel.DoesNotExist:
-            UserModel().set_password(password)
-            return
-        except UserModel.MultipleObjectsReturned:
-            user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
+            email = kwargs["email"]
+        except KeyError:
+            return None
+        password = kwargs["password"]
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password) is True:
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
 
-        if user.check_password(password) and self.user_can_authenticate(user):
-            return user
+    def get_user(self, email):
+        try:
+            return User.objects.get(pk=email)
+        except User.DoesNotExist:
+            return None
