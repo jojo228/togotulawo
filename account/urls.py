bcb3b92ffc892578af52f275410eb9d6b3c7e1f4
jojo228 <49480316+jojo228@users.noreply.github.com>
2023-07-05 21:39:25 +0000

    fdh

--- account/urls.py
+++ account/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path(
         "login/",
         auth_views.LoginView.as_view(
-            template_name="account/login.html",
+            template_name="login.html",
             authentication_form=AuthenticationFormWithEmail,
             next_page=reverse_lazy("main:accueil"),
         ),
