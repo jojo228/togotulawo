135ec54e07b49512e04aaa2a382420cd07fdf304
jojo228 <49480316+jojo228@users.noreply.github.com>
2023-07-08 14:27:11 +0000

    test

--- numerisation/apps.py
+++ numerisation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
+class NumerisationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'numerisation'
--- numerisation/urls.py
+++ numerisation/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from numerisation.views import *
 
 
 urlpatterns = [
