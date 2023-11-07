1b33930f1ab1e73a2c7ec0e4b8ab0e52f0245d7d
jojo228 <asslajosta@gmail.com>
2023-11-07 18:33:08 +0000

    updates

--- togotulawo/hosts.py
+++ togotulawo/hosts.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
-    host(r'gedus', 'gedus.urls', name='gedus'),
+    host(r'gedus', 'gedus.main.urls', name='gedus'),
     host(r'numerisation', 'numerisation.urls', name='numerisation'),
     host(r'tdm', 'tdm.urls', name='tdm'),
     host(r'ckeditor_uploader', 'ckeditor_uploader.urls', name='ckeditor_uploader'),
